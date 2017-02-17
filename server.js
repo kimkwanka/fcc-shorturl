ab5333a4cf435bc7f90465142469a897bb154792
Kim Kwanka <kimkwanka@gmail.com>
2017-02-17 14:01:16 +0100

    Fulfilled all User Stories (code super messy - need to refactor ASAP)

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongo = require('mongodb').MongoClient;
 const app = express();
 
 let db;
-let col;
+
 // Use process.env.PORT if set for Heroku, AWS, etc.
 const port = process.env.PORT || 8080;
 
@@ -33,22 +33,27 @@ const generateKey = url => (
   hashCode(url).toString(36)
 );
 
-const saveURL = (url) => {
+const saveURL = (url, cb) => {
+  console.log('Save what?', url);
   const urlDoc = {
     key: generateKey(url),
     url,
   };
+  const col = db.collection('shortenedUrls');
   col.find({
-    key: urlDoc.key
+    key: urlDoc.key,
   }).toArray((err1, docs) => {
     if (err1) throw err1;
     if (docs.length === 0) {
       col.insert(urlDoc, (err2) => {
         if (err2) throw err2;
         // URL was added
+        console.log('Added:', urlDoc);
+        cb(urlDoc);
       });
     } else {
-      // URL already in db
+      console.log('Already there:', urlDoc);
+      cb(urlDoc);
     }
   });
 };
@@ -70,25 +75,103 @@ app.use(stylus.middleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  saveURL('http://www.example.coms');
+  console.log('ROOT');
   res.render('index', {
     title: 'URL Shortener',
     url: 'https://XXXXXXXXXXX.com',
   });
 });
 
-app.get('/shorten', (req, res) => {
-  res.end();
+// Prevent browser's favicon request from triggering our microservice
+app.get('/favicon.ico', (req, res) => (
+  res.sendStatus(204)
+));
+
+app.get(/^\/(https?:\/\/)?(www\.)([\da-z-]+\.)+([\da-z-]{2,})|^\/(https?:\/\/)?(?!www.)([\da-z-]+\.)+([\da-z-]{2,})([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?/, (req, res) => { 
+//app.get(/^\/(https?:\/\/)?([\da-z\.-]+\.[a-z\.]{2,3}|[\d\.]+)([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?$/, (req, res) => {
+  console.log('Matched');
+  let ret = {
+    error: 'Invalid URL',
+  };
+  console.log( req.params);
+  let tmp = '';
+  // 8 capture groups in regex
+  for(let i=0; i<8 ;i++){ 
+    if(req.params[i]){
+      tmp += req.params[i];
+    }
+  }
+  req.param
+  const urlStr = tmp;
+  const col = db.collection('shortenedUrls');
+
+  col.find({
+    key: urlStr,
+  }).toArray((err1, docs) => {
+    if (err1) throw err1;
+    if (docs.length !== 0) {
+      res.redirect(docs[0].url);
+      //res.json({ redirect: docs[0].url });
+    } else {
+      if (true) {
+        // console.log('Not a key, but valid URL');
+        saveURL(urlStr, (r) => {
+          ret = {
+            url: r.url,
+            shortUrl: req.protocol + '://' + req.get('host') + '/' + r.key,
+          };
+          // console.log('ret', ret);
+          res.json(ret);
+        });
+      } else {
+        res.json(ret);
+      }
+    }
+  });
+});
+
+app.get('/:urlStr', (req, res) => {
+  console.log('Simple Match', req.params.urlStr);
+  const col = db.collection('shortenedUrls');
+
+  col.find({
+    key: req.params.urlStr,
+  }).toArray((err1, docs) => {
+    console.log('have I found it?');
+    if (err1) throw err1;
+    if (docs.length !== 0) {
+      let addition = '';
+      if(docs[0].url.indexOf('http') === -1){
+        addition = 'http://';
+      }
+      console.log('Simple redirect',err1, addition + docs[0].url)
+      res.redirect(addition + docs[0].url);
+      console.log('before crash');
+    } else {
+      console.log('before crash');
+      let ret = {
+        error: 'Invalid URL :O',
+      };
+      res.json(ret);
+    }
+  });
+  
 });
 
 app.get('*', (req, res) => {
+  
+  console.log('Should I ever land here?');
+  //let ret = {
+  //  error: 'Invalid URL',
+  //};
+  //res.json(ret);
+  //res.render('404', {});
   res.render('404', {});
 });
 
 mongo.connect(mongoUrl, (err, _db) => {
   if (err) throw err;
   db = _db;
-  col = db.collection('shortenedUrls');
   app.listen(port);
 });
 
