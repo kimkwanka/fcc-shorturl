4e18a5dd4d9f170c27cc7bcc0c184e20d1fe0a78
Kim Kwanka <kimkwanka@gmail.com>
2017-02-17 01:18:58 +0100

    Added mongodb and crude version of saving URL

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const stylus = require('stylus');
+const mongo = require('mongodb').MongoClient;
 
 const app = express();
 
+let db;
+let col;
 // Use process.env.PORT if set for Heroku, AWS, etc.
 const port = process.env.PORT || 8080;
+
+const mongoUrl = 'mongodb://localhost:27017/shorturl';
 
 const isValidURL = url => (
   url.search(/^(https?:\/\/)?([\da-z\.-]+\.[a-z\.]{2,6}|[\d\.]+)([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?$/) !== -1 // eslint-disable-line no-useless-escape
@@ -23,9 +28,30 @@ const hashCode = (str) => {
   return hash;
 };
 
+// Generate key by hashing the url and converting the result into a base 36 number to shorten it
 const generateKey = url => (
   hashCode(url).toString(36)
 );
+
+const saveURL = (url) => {
+  const urlDoc = {
+    key: generateKey(url),
+    url,
+  };
+  col.find({
+    key: urlDoc.key
+  }).toArray((err1, docs) => {
+    if (err1) throw err1;
+    if (docs.length === 0) {
+      col.insert(urlDoc, (err2) => {
+        if (err2) throw err2;
+        // URL was added
+      });
+    } else {
+      // URL already in db
+    }
+  });
+};
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
@@ -36,14 +62,15 @@ app.use(stylus.middleware({
   dest: path.join(__dirname, '/public'),
   compile: (str, filepath) => (
     stylus(str)
-      .set('filename', filepath)
-      .set('compress', true)
-    ),
+    .set('filename', filepath)
+    .set('compress', true)
+  ),
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+  saveURL('http://www.example.coms');
   res.render('index', {
     title: 'URL Shortener',
     url: 'https://XXXXXXXXXXX.com',
@@ -58,7 +85,16 @@ app.get('*', (req, res) => {
   res.render('404', {});
 });
 
-app.listen(port);
+mongo.connect(mongoUrl, (err, _db) => {
+  if (err) throw err;
+  db = _db;
+  col = db.collection('shortenedUrls');
+  app.listen(port);
+});
+
+process.on('SIGINT', () => {
+  db.close();
+});
 
 // export functions for testing in server-test.js
 module.exports = {
